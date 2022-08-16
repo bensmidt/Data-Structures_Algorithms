3f3ede6c6da70a99ab79b34c0b43522159fde365
Benjamin Smidt <benjamin.raymond.smidt@gmail.com>
2022-08-15 21:55:34 -0500

    started rod cutting.py

---
+++ Algos/Big4/Dynamic/rodcutting.py
@@ -0,0 +1,8 @@
+class Solution (object): 
+    def cut-rods(self, prices, rod_length): 
+        """Returns a list of optimal prices for cutting rods up to a rod length of n
+        Inputs: 
+        - prices: List of integers containing the price for  wood of length i
+        - rod_length: integer representing the maximum desired length of a rod
+        Returns: 
+        - optimal_rods: List of integers representing the optimal price for a piece of wood of length i
