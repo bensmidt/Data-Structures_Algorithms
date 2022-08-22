995bc9066dd697bf0d6b6856c416ac6a4ca73685
Benjamin Smidt <benjamin.raymond.smidt@gmail.com>
2022-08-21 21:57:33 -0500

    added bottom-up approach to rod cutting

--- Algos/Dynamic/RodCutting.py
+++ Algos/Dynamic/RodCutting.py
@@ -41,15 +41,53 @@ class Solution (object):
 
       optimal_prices[rod_length] = best_price
       return best_price
+  
+  def cut_rods_bottom_up (self, prices): 
+    """Returns a list of optimal prices for cutting rods up to a rod length of n
+    Inputs: 
+    - prices: List of integers containing the price for wood of length i
+    - optimal_prices: List of integers representing the optimal price for a rod of length i
+    """
+    optimal_prices = []
+    
+    for i in range(len(prices)):
+      # set best price to price of whole rod 
+      optimal_prices.append(prices[i])
+      best_price = prices[i]
+      # check for better prices
+      for j in range(i): 
+        best_price = max(best_price, optimal_prices[i-j] + optimal_prices[j])
+      # add best price 
+      optimal_prices[i] = best_price
+    
+    return optimal_prices
 
-def main(): 
-  prices = [0, 1, 5, 8, 9, 10, 17, 17, 20]
-  answers = [0, 1, 5, 8, 10, 13, 17, 18, 22]
+def test (prices, answers): 
   Sol = Solution()
-  optimal_prices = Sol.cut_rods_top_down (prices, len(prices) - 1)
+
+  optimal_prices = Sol.cut_rods_top_down (prices, len(prices) - 1) 
   for i in range(len(optimal_prices)): 
     assert optimal_prices[i] == answers[i]
+
+  optimal_prices = Sol.cut_rods_bottom_up(prices)
+  for i in range(len(optimal_prices)): 
+    assert optimal_prices[i] == answers[i]
+
+def main(): 
+  Sol = Solution()
+
+  # Test Case 1
+  prices = [0, 1, 5, 8, 9, 10, 17, 17, 20]
+  answers = [0, 1, 5, 8, 10, 13, 17, 18, 22]
+  test(prices, answers)
+
+  # Test Case 2
+  prices = [0, 1, 3, 4, 5, 7, 9, 10, 11]
+  answers = [0, 1, 3, 4, 6, 7, 9, 10, 12]
+  test(prices, answers)
+
   print("All Test Cases Passed!")
+  
 
 if  __name__ == "__main__": 
   main() 
